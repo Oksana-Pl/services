525f9c63b5042c7070c8befab0702d8959caa38a
Oksana-Pl <k00299681@student.tus.ie>
2026-06-09 23:24:28 +0100

    gallery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const swiper = new Swiper('.reviews--slider', {
 
   slidesPerView: 2,
-  spaceBetween: 50,
+  spaceBetween: 60,
     // Navigation arrows
     navigation: {
       nextEl: '.reviews--item-next',
       prevEl: '.reviews--item-prev',
     },
   });
+
+ 
