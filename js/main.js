678719e96889d137a407ebc10c201c0cd2aa3200
Oksana-Pl <k00299681@student.tus.ie>
2026-06-11 23:49:13 +0100

    media reviews

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ const swiper = new Swiper('.reviews--slider', {
 
   slidesPerView: 2,
   spaceBetween: 60,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    570: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    }
+  },
+  
     // Navigation arrows
     navigation: {
       nextEl: '.reviews--item-next',
