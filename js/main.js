a0f3b1ff522093bd7e90df74a9b21f20b295d0ad
Oksana-Pl <k00299681@student.tus.ie>
2026-06-24 21:42:12 +0100

    настройка форм

--- js/main.js
+++ js/main.js
@@ -37,4 +37,38 @@ menuList.classList.toggle('menu--open');
  });
 
 
-
+ const form = document.getElementById('contact-form');
+ const successMessage = document.getElementById('success-message');
+ const sendAgainBtn = document.getElementById('send-again');
+ 
+ form.addEventListener('submit', async (e) => {
+ 
+     e.preventDefault();
+ 
+     const data = new FormData(form);
+ 
+     const response = await fetch(form.action, {
+         method: form.method,
+         body: data,
+         headers: {
+             'Accept': 'application/json'
+         }
+     });
+ 
+     if(response.ok){
+ 
+         form.style.display = 'none';
+ 
+         successMessage.classList.add('active');
+ 
+         form.reset();
+     }
+ });
+ 
+ sendAgainBtn.addEventListener('click', () => {
+ 
+     successMessage.classList.remove('active');
+ 
+     form.style.display = 'flex';
+ 
+ });
